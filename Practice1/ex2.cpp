4676784e93de9d0e3910c3515348ce53acedc011
carlan-cloud <carlan@taltech.ee>
2026-02-18 11:14:21 +0200

    2 exercises done

---
+++ Practice1/ex2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <tuple>
+using namespace std;
+int main() {
+// Declare num1 as an integer and initialize it to 13.
+int num1 = 13;
+// Declare num2 as an integer and initialize it to 0.
+int num2 = 0;
+// Assign the value 5 to num1
+num1 = 5;
+// Assign the value of num1 to num2
+num2 = num1;
+cout << num1 << " " << num2;
+}
